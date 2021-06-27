9dc63ffb6806c18e8b8e636e6dbaf35d90af76a3
y5mei <meiyaowen@gmail.com>
2021-06-27 06:30:40 -0400

    删了 resource file ids.xml
    我没看懂 ids.xml 是啥，我删了就可以build 了，然后现在在 main activity 如果你点屏幕会crash，我猜测是因为你在layout里加了 onClick，然后试图转型 textView 到 editText，我留了注释，你改了就应该好了

--- app/src/main/java/com/example/firsttry/MainActivity.java
+++ app/src/main/java/com/example/firsttry/MainActivity.java
@@ -18,10 +18,11 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editText); // 你的main activity 的 xml file 里没有 editText, 只有一个 TextView，我猜TextView 应该不能被 cast 成 editText
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    // Java 把 field 放在 class {} 的最上面，然后再放 method；
     public final static String EXTRA_MESSAGE = "com.example.FirstTry.MESSAGE";
 }
